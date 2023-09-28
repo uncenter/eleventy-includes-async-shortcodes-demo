37931343089808df453b4f20751b0b825686cc6a
uncenter <47499684+uncenter@users.noreply.github.com>
2023-09-28 09:02:47 -0400

    feat: add sync version to compare, more examples with styling (#1)

--- eleventy.config.cjs
+++ eleventy.config.cjs
@@ -1,10 +1,10 @@
 module.exports = (eleventyConfig) => {
-  eleventyConfig.addAsyncShortcode("beep", async () => {
-    await new Promise((resolve) => {
-      setTimeout(resolve, 1000);
+    eleventyConfig.addAsyncShortcode("beep", async () => {
+        return "42";
     });
-    return "42";
-  });
+    eleventyConfig.addShortcode("beepSync", () => {
+        return "42";
+    });
 
-  return {};
+    return {};
 };
